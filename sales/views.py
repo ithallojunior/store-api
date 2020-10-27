79b80d045c1c23f31138e54f319dfc03fabf6d46
Ithallo Junior <ithallojunior@outlook.com>
2020-10-26 23:18:09 -0300

    feat: added view for the products

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from . import models, serializers, permissions
+
+
+class ProductsView(ModelViewSet):
+    """View for the products' CRUD."""
+
+    queryset = models.Products.objects.all()
+    permission_classes = [permissions.ProductsPermission]
+    serializer_class = serializers.ProductSerializer
+    lookup_field = 'name'
