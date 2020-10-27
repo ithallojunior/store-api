a3f587e57cbabd1a17ef60e717f2ed3c6d3db90b
Ithallo Junior <ithallojunior@outlook.com>
2020-10-27 02:00:49 -0300

    feat: added signal to update values

---
+++ sales/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from . import models
+
+
+@receiver(post_save, sender=models.Orders)
+def udpdate_product_quantity(sender, instance, **kwargs):
+    """Upadates the quantities aftes a order is finished."""
+
+    if instance.status == models.ORDERED:
+
+        diff_quantity = instance.quantity
+        product = instance.product
+        product.quantity -= diff_quantity
+        product.save()
