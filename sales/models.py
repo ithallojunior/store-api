a625c86c8189bebd698ebbc388eea71ad9e62016
Ithallo Junior <ithallojunior@outlook.com>
2020-10-26 23:16:36 -0300

    feat: added model for products and its migrations

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Products(models.Model):
+    """Modelates the products to be sold."""
+
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=500)
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
+    price = models.DecimalField(
+        max_digits=20,
+        decimal_places=2,
+        validators=[MinValueValidator(0.01)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
