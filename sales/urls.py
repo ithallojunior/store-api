80fbae1f1ddc2548a51e5727781bcc5d4bc6d4cf
Ithallo Guimarães <ithallojunior@outlook.com>
2021-08-16 21:21:03 -0300

    style: alter indentation on url

--- sales/urls.py
+++ sales/urls.py
@@ -11,9 +11,7 @@ router = DefaultRouter()
 router.register(r'products', views.ProductsView, basename='products')
 router.register(r'orders', views.OrdersView, basename='orders')
 router.register(
-    r'staff-order-control',
-    views.StaffOrdersView,
-    basename='staff-order-control'
+    r'staff-order-control', views.StaffOrdersView, basename='staff-order-control'
 )
 
 
