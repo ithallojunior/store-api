12eec8a5ea02dd404c7216f741883eb3ffbbc9f2
Ithallo Guimarães <ithallojunior@outlook.com>
2021-08-17 00:16:58 -0300

    chore: add tests run on script to be run

--- this.sh
+++ this.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+python /code/manage.py test
 python /code/manage.py loaddata /code/store_auth/fixture.json
 python /code/manage.py migrate
 python /code/manage.py runserver 0.0.0.0:8000
